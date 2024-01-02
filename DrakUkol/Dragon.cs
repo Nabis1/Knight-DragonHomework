9098c894b7b8d14147fbe0867b2c88b39b844d99
cerni <cerni@ALENA-LAPTOP>
2024-01-02 20:40:42 +0100

    Add project files.

---
+++ DrakUkol/Dragon.cs
@@ -0,0 +1,49 @@
+﻿
+namespace KnightDragonHomework
+{
+    public class Dragon
+    {
+        public string Name { get; set; }
+        public int NumberOfHeads { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Armor { get; set; }
+
+        public Dragon(string name, int headNumbers, int strength, int agility, int armor)
+        {
+            Name = name;
+            NumberOfHeads = headNumbers;
+            Strength = strength;
+            Agility = agility;
+            Armor = armor;
+        }
+        public void DisplayDragonInfo()
+        {
+            Console.WriteLine($"Dragon: {Name}");
+            Console.WriteLine($"Number of heads: {NumberOfHeads}");
+            Console.WriteLine($"Strength: {Strength}");
+            Console.WriteLine($"Agility: {Agility}");
+            Console.WriteLine($"Armor: {Armor}");
+        }
+        public void HitByKnight()
+        {
+            if (NumberOfHeads > 0)
+            {
+                NumberOfHeads--;
+
+                if (NumberOfHeads > 0)
+                {
+                    Console.WriteLine("Deathwing is shouting in pain : Where is my head!");
+                }
+                else
+                {
+                    Console.WriteLine("I'm defeated...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The dragon is already defeated!");
+            }
+        }
+    }
+}
---
+++ DrakUkol/Knight.cs
@@ -0,0 +1,41 @@
+﻿
+namespace KnightDragonHomework
+{
+    public class Knight
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int HitPoints { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Armor { get; set; }
+
+        public Knight(string name, string lastName, int hitPoints, int strength, int agility, int armor)
+        {
+            Name = name;
+            LastName = lastName;
+            HitPoints = hitPoints;
+            Strength = strength;
+            Agility = agility;
+            Armor = armor;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Knight: {Name} {LastName}");
+            Console.WriteLine($"Hit Points: {HitPoints}");
+            Console.WriteLine($"Strength: {Strength}");
+            Console.WriteLine($"Agility: {Agility}");
+            Console.WriteLine($"Armor: {Armor}");
+        }
+
+        public void AttackDragon(Dragon headNumbers)
+        {
+            while (headNumbers.NumberOfHeads > 0)
+            {
+                Console.WriteLine($"Oooooo, mighty knight {Name}, borned {LastName}, is attacking the dragon!");
+                headNumbers.HitByKnight();
+            }
+        }
+
+    }
+}
